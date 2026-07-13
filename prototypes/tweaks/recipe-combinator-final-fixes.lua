a7f856a6f477b17bcd942fd252b24af755238a06
Em Karisch <emii.khaos@gmail.com>
2026-07-13 17:30:05 +0200

    feat: tweak recipe_combinator

---
+++ prototypes/tweaks/recipe-combinator-final-fixes.lua
@@ -0,0 +1,11 @@
+local khaoslib_entity = require("__khaoslib__.prototypes.entity")
+local khaoslib_item = require("__khaoslib__.prototypes.item")
+local khaoslib_recipe = require("__khaoslib__.prototypes.recipe")
+
+if mods["recipe_combinator"] then
+  if mods["SchallCircuitGroup"] then
+    khaoslib_entity:load("arithmetic-combinator", "recipe-combinator-main"):set {subgroup = "circuit-combinator"} :commit()
+    khaoslib_item:load("recipe-combinator-main"):set {subgroup = "circuit-combinator"} :commit()
+    khaoslib_recipe:load("recipe-combinator-main"):set {subgroup = "circuit-combinator"} :commit()
+  end
+end
