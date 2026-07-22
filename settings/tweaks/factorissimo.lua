aabf566cff5dbbf869d7c9c99cc1a99042456be2
Em Karisch <emii.khaos@gmail.com>
2026-07-22 21:15:44 +0200

    fix: correct subgroup for factory circuit connector

--- settings/tweaks/factorissimo.lua
+++ settings/tweaks/factorissimo.lua
@@ -16,7 +16,7 @@ if mods["factorissimo-2-notnotmelon"] then
       type = "string-setting",
       name = "khaoscircuitry-factorissimo-circuit-subgroup",
       setting_type = "startup",
-      default_value = "circuit-combinator",
+      default_value = "circuit-connection",
       allowed_values = settings_constants.circuit_subgroups,
       order = "a[advanced]-b[factorissimo]-b[circuit-subgroup]",
       hidden = not mods["khaos-advanced-settings"]
